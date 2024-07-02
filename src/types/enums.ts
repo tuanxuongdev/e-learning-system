08458d77b2c28d5b2e5f60c9ffec9d5ef63bd1c5
tuanxuongdev <tuanxuongdev@gmail.com>
2024-07-02 23:20:15 +0700

    User schema

---
+++ src/types/enums.ts
@@ -0,0 +1,12 @@
+enum EUserStatus {
+  ACTIVE = "ACTIVE",
+  UNACTIVE = "UNACTIVE",
+  BANNED = "BANNED",
+}
+enum EUserRole {
+  ADMIN = "ADMIN",
+  USER = "USER",
+  EXPERT = "EXPERT",
+}
+
+export { EUserStatus, EUserRole };
